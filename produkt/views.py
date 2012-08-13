a5901a0a8bf0875e8de79304e0270da829e20c89
robertczech <robertczech@onet.eu>
2012-08-13 12:35:53 +0200

    paginacja

--- produkt/views.py
+++ produkt/views.py
@@ -2,9 +2,15 @@
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
 from produkt.models import *
-
+from django.core.paginator import Paginator
 
 def index(request):
     produkty = Produkt.objects.all()
+
+    p = Paginator(produkty, 2)
+    if request.GET :
+	page = p.page(request.GET.get('page'))
+    else :
+	page = p.page(1)
     return render_to_response('index.html',
-                             {'produkty': produkty})
+                             {'produkty': page })
